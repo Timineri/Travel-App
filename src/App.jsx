a8f1e272aa488d6a1c113f1d24b2367b701d7d6c
Timineri Okoko <tonyeokoko@outlook.com>
2024-06-08 09:14:16 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+// import React from "react";
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
