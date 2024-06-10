43090a3f1349d8f077161fe66717201e99b5bae1
Timineri Okoko <tonyeokoko@outlook.com>
2024-06-10 07:13:03 +0100

    Worked on the form components using controlled element to manage the state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,46 @@
 // import React from "react";
 
+import { useState } from "react";
+
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItems = { quantity, description, packed: false, id: Date.now() };
+    console.log(newItems);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  function handleChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handleQuantityChange(e) {
+    setQuantity(Number(e.target.value));
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onClick={handleSubmit}>
       <h3>What go you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={handleQuantityChange}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={handleChange}
+      />
       <button>Add</button>
     </form>
   );
